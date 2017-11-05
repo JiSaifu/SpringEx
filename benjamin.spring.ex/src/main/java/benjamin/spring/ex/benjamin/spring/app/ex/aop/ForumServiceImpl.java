6c4ca45ce7e4d979f91a1e9d9d71a09df4899c62
Saifu.ji <ji.saifu@gmail.com>
2017-11-05 20:06:12 +0900

    # Chapter6 开始 ## 采用JDK代理技术的横切实现

--- benjamin.spring.ex/src/main/java/benjamin/spring/ex/benjamin/spring/app/ex/aop/ForumServiceImpl.java
+++ benjamin.spring.ex/src/main/java/benjamin/spring/ex/benjamin/spring/app/ex/aop/ForumServiceImpl.java
@@ -2,13 +2,15 @@ package benjamin.spring.ex.benjamin.spring.app.ex.aop;
 
 import benjamin.spring.ex.benjamin.spring.app.ex.AbstractEx;
 
+import java.lang.reflect.Proxy;
+
 import static java.lang.Thread.sleep;
 
 public class ForumServiceImpl extends AbstractEx implements ForumService {
 
     public void removeTopic(int topicId) {
         // ↓具有横切逻辑特征的代码
-        PerformanceMonitor.begin(this.getClass().getName());
+        // PerformanceMonitor.begin(this.getClass().getName());
         System.out.println("模拟删除topic记录:" + topicId);
         try {
             sleep(20);
@@ -16,23 +18,33 @@ public class ForumServiceImpl extends AbstractEx implements ForumService {
             throw new RuntimeException(e);
         }
         // ↓具有横切逻辑特征的代码
-        PerformanceMonitor.end();
+        // PerformanceMonitor.end();
     }
 
     public void removeForum(int forumId) {
-        PerformanceMonitor.begin(this.getClass().getName());
+        // PerformanceMonitor.begin(this.getClass().getName());
         System.out.println("模拟删除forum记录:" + forumId);
         try {
             sleep(40);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        PerformanceMonitor.end();
+        // PerformanceMonitor.end();
     }
 
     @Override
     public void doEx() {
-        removeTopic(1);
-        removeForum(2);
+        // removeTopic(1);
+        // removeForum(2);
+
+        ForumService target = new ForumServiceImpl();
+        PerformanceHandler handler = new PerformanceHandler(target);
+
+        ForumService proxy = (ForumService) Proxy.newProxyInstance(
+                target.getClass().getClassLoader(),
+                target.getClass().getInterfaces(),
+                handler);
+        proxy.removeForum(1);
+        proxy.removeTopic(2);
     }
 }
---
+++ benjamin.spring.ex/src/main/java/benjamin/spring/ex/benjamin/spring/app/ex/aop/PerformanceHandler.java
@@ -0,0 +1,21 @@
+package benjamin.spring.ex.benjamin.spring.app.ex.aop;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class PerformanceHandler implements InvocationHandler {
+    private Object target;
+    PerformanceHandler(Object target) {
+        this.target = target;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args)
+            throws InvocationTargetException, IllegalAccessException {
+        PerformanceMonitor.begin(target.getClass().getName() + "." +
+        method.getName());
+        Object obj = method.invoke(target, args);
+        PerformanceMonitor.end();
+        return obj;
+    }
+}
